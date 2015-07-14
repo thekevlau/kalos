0be54a5d191980453478e372bda9959bb25372f8
Kevin Lau <thekevlau@gmail.com>
2015-07-14 00:03:03 -0400

    initial commit

--- README.md
+++ README.md
@@ -2,13 +2,8 @@
 
 Kalos is an IOT form analysis and correction tool that will help you reach your fitness goals faster and safer.
 
-
-
-
-
-
-
-
-
-
 Please note that Kalos is pending patent pending.
+
+# EvoThings Documentation
+
+Documentation for Evothings Studio.
---
+++ js/include-example-menu.js
@@ -0,0 +1,39 @@
+// Example menu.
+document.write(
+	'<div class="evo-box evo-content-index">'
+	+	'<script src="../js/include-evothings-logo.js"></script>'
+
+	+	'<a href="../examples/hello-world.html">Hello&nbsp;World</a><br/>'
+	+	'<a href="../examples/cordova-accelerometer.html">Cordova&nbsp;Accelerometer</a><br/>'
+	+	'<a href="../examples/hue-lights.html">Philips&nbsp;Hue&nbsp;Demo</a><br/>'
+
+	+	'<a href="../examples/ble-scan.html">BLE&nbsp;Scan</a><br/>'
+	+	'<a href="../examples/ibeacon-scan.html">iBeacon&nbsp;Scan</a><br/>'
+	+	'<a href="../examples/estimote-beacons.html">Estimote&nbsp;Beacons</a><br/>'
+	+	'<a href="../examples/estimote-nearables.html">Estimote&nbsp;Nearables</a><br/>'
+	+	'<a href="../examples/ble-discovery.html">BLE&nbsp;Discovery</a><br/>'
+
+	+	'<a href="../examples/ble-ti-sensortag-accelerometer.html">TI&nbsp;SensorTag&nbsp;Accelerometer</a><br/>'
+	+	'<a href="../examples/ble-ti-sensortag-sensors.html">TI&nbsp;SensorTag&nbsp;Sensors</a><br/>'
+	+	'<a href="../examples/ble-ti-sensortag-cc2650-demo.html">TI&nbsp;SensorTag&nbsp;CC2650&nbsp;Demo</a><br/>'
+	+	'<a href="../examples/ble-ti-sensortag-cc2541-demo.html">TI&nbsp;SensorTag&nbsp;CC2541&nbsp;Demo</a><br/>'
+
+	+	'<a href="../examples/mbed-custom-gap.html">mbed&nbsp;Custom&nbsp;GAP</a><br/>'
+	+	'<a href="../examples/mbed-custom-gatt.html">mbed&nbsp;Custom&nbsp;GATT</a><br/>'
+	+	'<a href="../examples/nordic-nRF51-ble.html">Nordic&nbsp;nRF51&nbsp;BLE</a><br/>'
+	+	'<a href="../examples/nordic-nRF51822-ble.html">Nordic&nbsp;nRF51822&nbsp;BLE</a><br/>'
+
+	+	'<a href="../examples/arduino-led-onoff-tcp.html">Arduino&nbsp;LED&nbsp;On/Off&nbsp;TCP</a><br/>'
+	+	'<a href="../examples/arduino-led-onoff-ble.html">Arduino&nbsp;LED&nbsp;On/Off&nbsp;BLE</a><br/>'
+	+	'<a href="../examples/arduino-ble.html">Arduino&nbsp;BLE</a><br/>'
+	+	'<a href="../examples/arduino-scriptable-tcp.html">Arduino&nbsp;Scriptable&nbsp;TCP</a><br/>'
+
+	+	'<a href="../examples/redbearlab-simplecontrol.html">RedBearLab&nbsp;Simple&nbsp;Control</a><br/>'
+	+	'<a href="../examples/redbearlab-simplechat.html">RedBearLab&nbsp;Simple&nbsp;Chat</a><br/>'
+
+	+	'<a href="../examples/mediatek-linkit.html">MediaTek&nbsp;Linkit&nbsp;ONE&nbsp;Position</a><br/>'
+	+	'<a href="../examples/lightbluebean-basic.html">LightBlue&nbsp;Bean&nbsp;Basic</a><br/>'
+	+	'<a href="../examples/rfduino-led-onoff.html">RFduino&nbsp;Led&nbsp;On/Off</a><br/>'
+	+	'<a href="../examples/bluno-helloworld.html">Bluno&nbsp;Hello&nbsp;World</a><br/>'
+
+	+ '</div>')
